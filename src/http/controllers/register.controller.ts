8d044c625e3044023a0f41e03d8c223a6c85ede2
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-17 17:41:25 -0300

    feat(app): handling errors   Addressing global and non-global application errors.

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -2,6 +2,7 @@ import type { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
 
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 import { RegisterUseCase } from '@/use-cases/register'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -22,8 +23,12 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       email,
       password,
     })
-  } catch {
-    return reply.status(409).send()
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    throw error
   }
 
   return reply.status(201).send()
