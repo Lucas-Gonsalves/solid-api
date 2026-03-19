ed0630c12e3d893d4173d715acfa6c27438d683a
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-19 11:44:49 -0300

    feat(repository): create get user profile   The `get user profile` repository was created to collect user information. Tests and validation were implemented.

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,39 @@
+import { hash } from 'bcryptjs'
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { GetUserProfileUseCase } from './get-user-profile'
+
+describe('Get User Profile Use Case', () => {
+  let usersRepository: InMemoryUsersRepository
+  let sut: GetUserProfileUseCase
+
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+
+  it('should be able to get a user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'Jhon Doe',
+      email: 'jhondoe@gmail.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.name).toEqual('Jhon Doe')
+  })
+
+  it('should not be able to get a user profile with wrong id', async () => {
+    expect(() =>
+      sut.execute({
+        userId: 'not-existing-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
