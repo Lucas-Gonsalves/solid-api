5b75886e1c2eb24a4152e73ede8d0fe42117a178
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-23 16:16:44 -0300

    feat(http) create gyms controllers   The controllers for create, nearby, and search for gyms have been created.

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,15 @@
+import type { FastifyInstance } from 'fastify'
+
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+
+import { authenticate } from './authenticate.controller'
+import { profile } from './profile.controller'
+import { register } from './register.controller'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/users', register)
+  app.post('/session', authenticate)
+
+  /** Authenticated */
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
+}
