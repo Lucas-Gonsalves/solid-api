556da155d1a444e6f93632ad0568aa3c7112fc64
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-23 15:04:54 -0300

    feat(tests): creating e2e testing environment   The environment was created using Vitest to perform end-to-end tests. The first test performed was on the register controller.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,27 @@
-import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  test: {
+    dir: 'src',
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/use-cases',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers',
+          setupFiles: ['./vitest.e2e.setup.ts'],
+        },
+      },
+    ],
+  },
 })
