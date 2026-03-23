78b9b8e4e75bf2d136964eba6c0966f5b091ac03
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-23 17:46:10 -0300

    feat(http): create check-ins tests   End-to-end (e2e) tests have been created for the create, history, metrics, and validate controllers of the check-ins.

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,38 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '@/app'
+import { prisma } from '@/lib/prisma'
+import { createAndAuthenticateUser } from '@/utils/tests/create-and-authenticate-user'
+
+describe('Create Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'JavaScript Gym',
+        latitude: -26.4567697,
+        longitude: -48.9380844,
+      },
+    })
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -26.4567697,
+        longitude: -48.9380844,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
