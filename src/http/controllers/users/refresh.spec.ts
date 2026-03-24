5e58e9b934034bc5110ecfc0321fee35c5138853
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-24 15:34:59 -0300

    feat(http): create refresh controller   The controller responsible for refreshing the tokens has been created if the refresh token is successfully validated during the verification process. The test for the refresh controller has also been created and is working correctly.

---
+++ src/http/controllers/users/refresh.spec.ts
@@ -0,0 +1,44 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '@/app'
+
+describe('Refresh Token (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to refresh a token', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const authResponse = await request(app.server).post('/session').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const cookies = authResponse.get('Set-Cookie')!
+
+    const refreshResponse = await request(app.server)
+      .patch('/token/refresh')
+      .set('Cookie', cookies)
+      .send()
+
+    expect(refreshResponse.statusCode).toEqual(200)
+
+    expect(refreshResponse.statusCode).toEqual(200)
+    expect(refreshResponse.body).toMatchObject({
+      token: expect.any(String),
+    })
+    expect(refreshResponse.get('Set-Cookie')).toEqual([
+      expect.stringContaining('refreshToken='),
+    ])
+  })
+})
